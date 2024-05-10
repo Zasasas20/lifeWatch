92da13c07462d20c800408789b7f8d15cde16824
Zasasas20 <zaidassim2013@gmail.com>
2024-05-10 17:18:04 +0200

    Fixed navigation bar and color thematic

--- App/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/App.AssemblyInfo.cs
+++ App/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/App.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6b392ccaa6100a259b85718251268c38971d933b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ad7678c92efbb0d04a9869734ddf9c1193f85aa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
